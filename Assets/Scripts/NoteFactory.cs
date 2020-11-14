04b8a5cd330b0a182e8ff70850b9091541d2806e
Túlio Lages <tuliolages@joystreet.com.br>
2020-11-14 12:04:24 -0300

    Add a initial notefactory implementation

---
+++ Assets/Scripts/NoteFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteFactory : MonoBehaviour
+{
+
+    public GameObject notePrefab;
+
+    Queue<KeyCode> myQueue = new Queue<KeyCode>();
+
+    void Start()
+    {
+        AddNotesToQueue();
+        AddNotesToQueue();
+        AddNotesToQueue();
+        AddNotesToQueue();
+        AddNotesToQueue();
+
+        InvokeRepeating("InstantiateNote", 1f, 1.0f);
+    }
+
+    void InstantiateNote()
+    {
+        KeyCode currentNote = myQueue.Dequeue();
+
+        GameObject newNoteInstance = Instantiate(
+            notePrefab,
+            new Vector3(transform.position.x, transform.position.y, transform.position.z),
+            Quaternion.identity
+        );
+        newNoteInstance.transform.parent = gameObject.transform;
+        newNoteInstance.GetComponent<NoteObject>().key = currentNote;
+    }
+
+    void AddNotesToQueue()
+    {   
+        myQueue.Enqueue(KeyCode.UpArrow);
+        myQueue.Enqueue(KeyCode.DownArrow);
+        myQueue.Enqueue(KeyCode.LeftArrow);
+        myQueue.Enqueue(KeyCode.RightArrow);
+        myQueue.Enqueue(KeyCode.UpArrow);
+        myQueue.Enqueue(KeyCode.DownArrow);
+        myQueue.Enqueue(KeyCode.LeftArrow);
+        myQueue.Enqueue(KeyCode.RightArrow);
+    }
+}
