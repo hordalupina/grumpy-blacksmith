3a6a6efaa18263e7c11db4bf90ca305ee5476439
Túlio Lages <tuliolages@joystreet.com.br>
2020-11-13 23:31:38 -0300

    initial commit

---
+++ Assets/Scripts/Collider1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider1 : MonoBehaviour
+{
+    void Start() {
+    
+    }
+ 
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+
+    public bool canBePressed;
+    public string key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canBePressed && Input.GetKey(key))
+        {
+            print("up arrow key is held down");
+            gameObject.SetActive(false);
+            canBePressed = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        canBePressed = true;
+        Debug.Log(other.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        canBePressed = false;
+        Debug.Log(other.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+    }
+}
